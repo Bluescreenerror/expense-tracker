fb7a93b7cbd5562f65919b45fc378607f1cb6194
Bluescreenerror <jeeshaan.j@gmail.com>
2024-11-22 17:42:14 -0500

    demo-app
    Commit to master

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Header from "./components/Header";
+import { Balance } from "./components/Balance";
+import { Expenses } from "./components/Expenses";
+import { TransactionList } from "./components/TransactionList";
+import { AddTransaction } from "./components/AddTransaction";
 
-function App() {
+import "./App.css";
+
+const App = () => {
+  const [transactions, setTransactions] = useState([]);
+
+  // Delete a transaction
+  const deleteTransaction = (id) => {
+    setTransactions((prev) => prev.filter((transaction) => transaction.id !== id));
+  };
+
+  // Add a transaction
+  const addTransaction = (transaction) => {
+    setTransactions((prev) => [transaction, ...prev]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <div className="container">
+        <Balance transactions={transactions} />
+        <Expenses transactions={transactions} />
+        <TransactionList transactions={transactions} deleteTransaction={deleteTransaction} />
+        <AddTransaction addTransaction={addTransaction} />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
